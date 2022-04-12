467aa8c8fe42f5e1f3f39abfd3bb12ebf5ebcff8
qufan <allen5858@163.com>
2022-04-12 18:34:22 +0800

    subscribe

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { watch } from 'vue'
 import { useStore } from 'vuex'
 import * as Cesium from 'cesium'
 // import * as widgets from 'cesium/Widgets/widgets.css'
@@ -8,15 +9,23 @@ let viewer = undefined
 let socket = store.state.socket.ws
 onMounted(() => {
   viewer = new Cesium.Viewer('cesiumContainer')
-  if(socket == undefined){
+  if (socket == undefined) {
     store.dispatch('socket/socketInit')
   }
 })
+
+store.subscribe((mutation) => {
+  console.log(mutation)
+  if(mutation.type == 'socket/receive'){
+    console.log(mutation.payload)
+  }
+})
+// watch()
 </script>
 <template>
   <div id="cesiumContainer"></div>
 </template>
 <script>
-// export default {}
+export default {}
 </script>
 <style></style>
